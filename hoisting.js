f0398ba4707c8cec466efacabbef8d2d069f0354
Zaid Shawqi <zeid.shawqi@gmail.com>
2020-04-06 19:43:41 +0000

    Automatically backed up by Learn

--- hoisting.js
+++ hoisting.js
@@ -1,7 +1,13 @@
 callme();
 function callme() {
-  console.log("maybe");
 
+  console.log("I just met you...");
+  console.log("and this is crazy..");
+  console.log("but here's my number..");
+  console.log("so call me");
+
+  return lyric;
+  var lyric = "maybe";
 
 }
 
